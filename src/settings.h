d50e5a3a331fcf14c9b06da7fdce443eacecd1de
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2020-08-20 14:08:43 +0200

    Update translation files (for blacklist/filter-list renaming and do-not-disturb feature).

--- src/settings.h
+++ src/settings.h
@@ -6,8 +6,8 @@
 #define __SETTINGS_H__
 
 #define NOTIFICATIONS_SCHEMA                  "org.ayatana.indicator.notifications"
-#define NOTIFICATIONS_KEY_BLACKLIST           "filter-list"
-#define NOTIFICATIONS_KEY_BLACKLIST_HINTS     "filter-list-hints"
+#define NOTIFICATIONS_KEY_FILTER_LIST           "filter-list"
+#define NOTIFICATIONS_KEY_FILTER_LIST_HINTS     "filter-list-hints"
 #define NOTIFICATIONS_KEY_CLEAR_MC            "clear-on-middle-click"
 #define NOTIFICATIONS_KEY_DND                 "do-not-disturb"
 #define NOTIFICATIONS_KEY_HIDE_INDICATOR      "hide-indicator"
